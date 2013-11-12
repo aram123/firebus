1415b1e98dd421f4186c7f84eabe35f336a281df
Alberto Rascon <aram123@gmail.com>
2013-11-12 11:32:30 -0700

    Added stations to indexeddb

--- public_html/js/indexedDb.js
+++ public_html/js/indexedDb.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (!indexedDBOk)
         return;
 
-    var openRequest = indexedDB.open("bustoppppp", 6);
+    var openRequest = indexedDB.open("bustoppppppppp", 10);
 
     openRequest.onupgradeneeded = function(e) {
         var thisDB = e.target.result;
@@ -20,9 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!thisDB.objectStoreNames.contains("busstop")) {
             var os = thisDB.createObjectStore("busstop", {autoIncrement: true});
             //index on bus stop name
-            os.createIndex("bus_stop_name", "bus_stop_name", {unique: false});
+            os.createIndex("route", "route", {unique: false});
             os.createIndex("origin", "origin", {unique: false});
-            os.createIndex("destination", "destination", {unique: false});
+            os.createIndex("is_station", "is_station", {unique: false});
+            os.createIndex("bus_stop_name", "bus_stop_name", {unique: false});
+            os.createIndex("consecutive", "consecutive", {unique: false});
         }
         initDb = true;
     }
@@ -37,20 +39,23 @@ document.addEventListener("DOMContentLoaded", function() {
         //Do something for the error
     }
 
-
 }, false);
 
 function fillBusStop(){
     console.log('maybe false...');
     if(initDb){
         console.log('true!');
-        addBusStop('nf', 'heroes', 'heroes1');
-        addBusStop('nf', 'dah', 'duh');   
+        addBusStop(1, "28.632276,-106.070513", "Independencia",1,1);
+        addBusStop(1, "28.636513,-106.076306", "Catedral",1,2);
+        addBusStop(1, "28.632264,-106.072998", "Quinta Gameros",1,3);
+        
+        initDB = false;
     }
 }
 
-function addBusStop(busStopName, origin, destination) {
-    console.log("Añadiendo " + busStopName+ " " + origin + " " + destination + "...");
+function addBusStop(route, origin, is_station, bus_stop_name, consecutive) {
+    console.log("Añadiendo " + route+ " " + origin + " " + is_station + " " + bus_stop_name + " " + consecutive + "...");
+    
 
     //Get a transaction
     //default for OS list is all, default for type is read
@@ -60,9 +65,11 @@ function addBusStop(busStopName, origin, destination) {
 
     //Define a person
     var busStopObj = {
-        'bus_stop_name': busStopName,
-        'origin': origin,
-        'destination': destination
+        'route' : route, 
+        'origin' : origin,
+        'is_station' : is_station,
+        'bus_stop_name' : bus_stop_name,
+        'consecutive' : consecutive
     }
 
     //Perform the add
